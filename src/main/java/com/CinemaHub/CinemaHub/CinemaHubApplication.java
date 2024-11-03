9325af65ec86131acb6b1284a45b445470067854
Amaan Ali Khan <amaanali.amaanali@gmail.com>
2024-11-04 02:16:19 +0530

    initial commit

---
+++ src/main/java/com/CinemaHub/CinemaHub/CinemaHubApplication.java
@@ -0,0 +1,13 @@
+package com.CinemaHub.CinemaHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CinemaHub/CinemaHub/CinemaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CinemaHub.CinemaHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
