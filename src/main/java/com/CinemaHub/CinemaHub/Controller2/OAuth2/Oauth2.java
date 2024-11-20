f7d8f3fea8d5eedba523dd9e7d1c538ee172fc90
Amaan Ali Khan <amaanali.amaanali@gmail.com>
2024-11-20 20:54:19 +0530

    Implemented OAuth2

---
+++ src/main/java/com/CinemaHub/CinemaHub/Controller2/OAuth2/Oauth2.java
@@ -0,0 +1,28 @@
+package com.CinemaHub.CinemaHub.Controller2.OAuth2;
+
+import com.CinemaHub.CinemaHub.Entity.User;
+import com.CinemaHub.CinemaHub.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Oauth2")
+public class Oauth2 {
+
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/createAdmin")
+    public void createAdminUser(@RequestBody User user) {
+        userService.saveNewAdminUser(user);
+    }
+
+    @PostMapping("/createUser")
+    public void createUserUser(@RequestBody User user) {
+        userService.saveNewUser(user);
+    }
+}
