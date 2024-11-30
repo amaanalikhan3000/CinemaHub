0f432be96e4d12efa7d637f6af7924b760ef3b45
Amaan Ali Khan <amaanali.amaanali@gmail.com>
2024-11-30 18:08:04 +0530

    Updated and fixed error for @PostMapping("/movie") and @DeleteMapping("movieId/{movieId}") Endpoints.

--- src/main/java/com/CinemaHub/CinemaHub/Entity/Movie.java
+++ src/main/java/com/CinemaHub/CinemaHub/Entity/Movie.java
@@ -4,26 +4,43 @@ package com.CinemaHub.CinemaHub.Entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Entity
 @Data
 @Table(name = "movie")
 public class Movie {
+
+    @Valid
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer movieId;
 
+
+
+    @NotNull
+    @NotBlank(message = "Title is required and cannot be blank")
     private String title;
+
+    @NotNull
+    @NotBlank(message = "Description is required and cannot be blank")
     private String description;
+
+    @Min(value = 1, message = "Duration must be at least 1 minute")
     private int durationInMin;
+
+    @NotBlank(message = "Language is required and cannot be blank")
     private String language;
+
+    @NotNull(message = "Release date is required")
     private LocalDateTime releaseDate;
+    // Not reqd
     private String country;
+    // not reqd
     private String genre;
-
-
-
-
 
 }
